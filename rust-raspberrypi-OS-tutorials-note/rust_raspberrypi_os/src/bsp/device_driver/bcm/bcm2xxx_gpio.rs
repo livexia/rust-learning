2b3357c7e816129655bfee39373e17a6977a21e5
livexia <istillalive@live.cn>
2020-09-30 07:46:05 +0800

    WIP: 06

--- rust-raspberrypi-OS-tutorials-note/rust_raspberrypi_os/src/bsp/device_driver/bcm/bcm2xxx_gpio.rs
+++ rust-raspberrypi-OS-tutorials-note/rust_raspberrypi_os/src/bsp/device_driver/bcm/bcm2xxx_gpio.rs
@@ -7,7 +7,109 @@
 use crate::{
     bsp::device_driver::common::MMIODerefWrapper, cpu, driver, synchronization,
     synchronization::NullLock,
-}
+};
 
 use register::{mmio::*, register_bitfields, register_structs};
 
+// GPIO registers.
+//
+// Descriptions taken from
+// https://github.com/raspberrypi/documentation/files/1888662/BCM2837-ARM-Peripherals.-.Revised.-.V2-1.pdf
+register_bitfields! {
+    u32,
+
+    GPFSEL1 [
+        FSEL15 OFFSET(15) NUMBITS(3) [
+            Input = 0b000,
+            Output = 0b001,
+            AltFunc0 = 0b100
+        ],
+
+        FSEL14 OFFSET(12) NUMBITS(3) [
+            Input = 0b000,
+            Output = 0b001,
+            AltFunc0 = 0b100
+        ]
+    ],
+
+    GPPUDCLK0 [
+        PUDCLK15 OFFSET(15) NUMBITS(1) [
+            NoEffect = 0,
+            AssertClock = 1
+        ],
+
+        PUDCLK14 OFFSET(14) NUMBITS(1) [
+            NoEffect = 0,
+            AssertClock = 1
+        ]
+    ]
+}
+
+register_structs! {
+    #[allow(non_snake_case)]
+    RegisterBlock {
+        (0x00 => GPFSEL0: ReadWrite<u32>),
+        (0x04 => GPFSEL1: ReadWrite<u32, GPFSEL1::Register>),
+        (0x08 => GPFSEL2: ReadWrite<u32>),
+        (0x0C => GPFSEL3: ReadWrite<u32>),
+        (0x10 => GPFSEL4: ReadWrite<u32>),
+        (0x14 => GPFSEL5: ReadWrite<u32>),
+        (0x18 => _reserved1),
+        (0x94 => GPPUD: ReadWrite<u32>),
+        (0x98 => GPPUDCLK0: ReadWrite<u32, GPPUDCLK0::Register>),
+        (0x9C => GPPUDCLK1: ReadWrite<u32>),
+        (0xA0 => @END),
+    }
+}
+
+type Register = MMIODerefWrapper<RegisterBlock>;
+
+
+pub struct GPIO {
+    register: NullLock<Register>,
+}
+
+impl GPIO {
+    /// Create an instance.
+    ///
+    /// # Safety
+    ///
+    /// - The user must ensure to provide the correct `base_addr`.
+    pub const unsafe fn new(base_addr: usize) -> Self {
+        Self {
+            register: NullLock::new(Registers::new(base_addr)),
+        }
+    }
+
+    /// Map PL011 UART as standard output.
+    ///
+    /// TX to pin 14
+    /// RX to pin 15
+    pub fn map_plo11_uart(&self) {
+        let mut r = &self.register;
+        r.lock(|registers| {
+            registers
+                .GPFSEL1
+                .modify(GPFSEL1::FSEL14::AltFunc0 + GPFSEL1::FSEL15::AltFunc0);
+
+            registers.GPPUD.set(0);
+            cpu::spin_for_cycles(150);
+
+            registers
+                .GPPUDCLK0
+                .write(GPPUDCLK0::PUDCLK14::AssertClock + GPPUDCLK0::PUDCLK15::AssertClock);
+            cpu::spin_for_cycles(150);
+
+            registers.GPPUDCLK0.set(0);
+        })
+    }
+}
+
+
+use synchronization::interface::Mutex;
+
+impl driver::interface::DeviceDriver for GPIO {
+    fn compatible(&self) -> &str {
+        "BCM GPIO"
+    }
+}
