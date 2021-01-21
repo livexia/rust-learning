94f4c15f8b930ed8ca4de9d5650bfc75b33aac69
livexia <istillalive@live.cn>
2021-01-21 14:39:15 +0800

    finish variable

---
+++ a_half_hour_to_learn_rust/variable01/src/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+    // let
+
+    let x; // declare
+    x = 55;
+    println!("{}", x);
+
+    //same as
+    let x = 55;
+    println!("{}", x);
+
+    // declare with type
+    let x: i32 = 55;
+    println!("i32 x: {}", x);
+
+    // can not x = x + 3;
+    // but can do let x = x + 3;
+    let x = x + 3;
+    println!("{}", x);
+
+    // tuple
+    let pair = ('a', '5');
+    println!("{}", pair.0);
+    println!("{}", pair.1);
+
+    // annotate the type of tuple
+    let pair: (char, i32);
+    pair = ('b', 6);
+    println!("{}, {}", pair.0, pair.1);
+
+    // destructured tuple
+    let (pair1, pair2) = pair;
+    println!("{}, {}", pair1, pair2);
+
+    // when function return tuple
+    let x:[i32; 6] = [0, 1, 2, 3, 4, 5];
+    let (part1, part2) = x.split_at(3);
+    println!("{:?}, {:?}", part1, part2);
+
+}
