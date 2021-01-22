6198aa65bfbf57693fe03170837a42762f00a798
livexia <istillalive@live.cn>
2021-01-23 02:10:56 +0800

    Save

--- leetcode/leetcode_04_12_paths-with-sum-lcci/src/main.rs
+++ leetcode/leetcode_04_12_paths-with-sum-lcci/src/main.rs
@@ -48,12 +48,56 @@ impl TreeNode {
 fn main() {
     println!("Hello, world!");
     let tree = create_tree(&mut vec![Some(5), Some(4), Some(11), Some(7), None, None, Some(2), None, None, None, Some(8), Some(13), None, None, Some(4), Some(5), None, None, Some(1), None, None]);
+    // let tree = create_tree(&mut vec![Some(1)]);
+    // let tree = create_tree(&mut vec![Some(0), Some(1), Some(1)]);
+    // let tree = create_tree(&mut vec![Some(-2), None, Some(-3)]);
+    // let tree = create_tree(&mut vec![Some(1), Some(-2), None, None, Some(3)]);
+    let tree = create_tree(&mut vec![Some(1), Some(-2), Some(1), Some(-1), None, None, None, Some(3), None, None, Some(-3), Some(-2)]);
+    let tree = create_tree(&mut vec![Some(1), Some(0), Some(1), Some(0), None, None, Some(1), None, None, Some(2), Some(-1), None, None, Some(0), None, None, Some(1),Some(0), Some(-1), None, None, Some(0), None, None, Some(-1), Some(1), None, None, Some(0)]);
     println!("{:?}", tree);
+    println!("{:?}", path_sum(tree, 2));
 }
 
 pub fn path_sum(root: Option<Rc<RefCell<TreeNode>>>, sum: i32) -> i32 {
-    0
+    if root == None {
+        return 0;
+    }
+    pre_order_travel(&root, sum, &mut Vec::new(), 0, 0)
 }
+
+pub fn pre_order_travel(node: &Option<Rc<RefCell<TreeNode>>>, sum: i32, traveled: &mut Vec<i32>, actual_sum: i32, result: i32) -> i32 {
+    let mut result = result;
+    let tree_node = node.as_ref().unwrap().as_ref().borrow();
+    traveled.push(tree_node.val);
+    println!("{:?}", traveled);
+    if actual_sum + tree_node.val == sum {
+        println!("Ok");
+        result += 1;
+    }
+    let mut traveled_copy = traveled.clone();
+    let mut actual_sum_copy = actual_sum + tree_node.val;
+    actual_sum_copy -= traveled_copy.remove(0);
+    while  actual_sum_copy != sum && traveled_copy.len() > 0 {
+        actual_sum_copy -= traveled_copy.remove(0);
+    }
+    if actual_sum_copy == sum && traveled_copy.len() > 0 {
+        println!("Ok1");
+        println!("c: {:?}", traveled_copy);
+
+        result += 1;
+        println!("{:?}", traveled);
+    }
+
+    if tree_node.left != None{
+        result = pre_order_travel(&tree_node.left, sum, traveled, actual_sum + tree_node.val, result);
+    }
+    if tree_node.right != None {
+        result = pre_order_travel(&tree_node.right, sum, traveled, actual_sum + tree_node.val, result);
+    }
+    traveled.pop();
+    result
+}
+
 
 pub fn create_tree(a: &mut Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
     if a.len() == 0 {
