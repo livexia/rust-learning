68c04801d3c164bd78e91028ee687da72248cfeb
livexia <istillalive@live.cn>
2021-01-29 15:08:47 +0800

    upload

---
+++ leetcode/leetcode_144/src/lib.rs
@@ -0,0 +1,77 @@
+/*
+你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+
+示例 1：
+输入：root = [1,null,2,3]
+输出：[1,2,3]
+
+示例 2：
+输入：root = []
+输出：[]
+
+示例 3：
+输入：root = [1]
+输出：[1]
+
+示例 4：
+输入：root = [1,2]
+输出：[1,2]
+
+示例 5：
+输入：root = [1,null,2]
+输出：[1,2]
+ 
+提示：
+树中节点数目在范围 [0, 100] 内
+-100 <= Node.val <= 100
+
+进阶：递归算法很简单，你可以通过迭代算法完成吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-preorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+use std::rc::Rc;
+use std::cell::RefCell;
+
+pub fn preorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut res = Vec::new();
+    traversal(root, &mut res);
+    res
+}
+
+pub fn traversal(node: Option<Rc<RefCell<TreeNode>>>, res: &mut Vec<i32>) {
+    if let Some(n) = node {
+
+        res.push(n.borrow().val);
+        traversal(n.borrow_mut().left.clone(), res);
+        traversal(n.borrow_mut().right.clone(), res);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
