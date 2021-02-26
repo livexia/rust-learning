9613bfa7e31be637d78d97ae6e37961fac737008
livexia <istillalive@live.cn>
2021-02-27 00:35:12 +0800

    20210227 01 bit manipulation

---
+++ leetcode/leetcode_1290/src/lib.rs
@@ -0,0 +1,74 @@
+/**
+给你一个单链表的引用结点 head。链表中每个结点的值不是 0 就是 1。已知此链表是一个整数数字的二进制表示形式。
+
+请你返回该链表所表示数字的 十进制值 。
+
+示例 1：
+输入：head = [1,0,1]
+输出：5
+解释：二进制数 (101) 转化为十进制数 (5)
+
+示例 2：
+输入：head = [0]
+输出：0
+
+示例 3：
+输入：head = [1]
+输出：1
+
+示例 4：
+输入：head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+输出：18880
+
+示例 5：
+输入：head = [0,0]
+输出：0
+
+提示：
+
+链表不为空。
+链表的结点总数不超过 30。
+每个结点的值不是 0 就是 1。
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/convert-binary-number-in-a-linked-list-to-integer
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+
+pub fn get_decimal_value(head: Option<Box<ListNode>>) -> i32 {
+    let mut res = 0;
+    let mut temp = &head;
+    while let Some(node) = temp {
+        res = res << 1;
+        res |= node.val;
+        temp = &node.next;
+    }
+
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
