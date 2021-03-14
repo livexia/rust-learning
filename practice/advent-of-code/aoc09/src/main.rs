28dfabc3d509c7af25ed00b143f2e13025fe1fc4
livexia <istillalive@live.cn>
2021-03-14 16:01:12 +0800

    aoc08 use linked list and more idiomatic rust

--- practice/advent-of-code/aoc09/src/main.rs
+++ practice/advent-of-code/aoc09/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Read, Write};
+use std::{env::{var, var_os}, fmt, io::{self, Read, Write}};
 use std::error::Error;
 use std::result;
 
@@ -22,6 +22,7 @@ fn main() -> Result<()> {
     println!("{}, {}", max_player, max_marble);
 
     part1(max_player, max_marble)?;
+    part1_way2(max_player, max_marble)?;
     part2(max_player, max_marble * 100)?;
    
     Ok(())
@@ -62,7 +63,130 @@ fn part1(max_player: usize, max_marble: usize) -> Result<()> {
     Ok(())
 }
 
+fn part1_way2(max_player: usize, max_marble: usize) -> Result<()> {
+    let mut players = vec![Player::default(); max_player];
+    play_game(&mut players, &mut Circle::new(), max_marble as u32);
+    writeln!(
+        io::stdout(), 
+        "winning score (part 1): {:?}", 
+        players.iter().map(|p| p.points).max().unwrap()
+    )?;
+    Ok(())
+}
+
 fn part2(max_player: usize, max_marble: usize) -> Result<()> {
-    writeln!(io::stdout(), "winning score (part 2): {:?}", todo!())?;
-    todo!("timeout");
+    let mut players = vec![Player::default(); max_player];
+    play_game(&mut players, &mut Circle::new(), max_marble as u32);
+    writeln!(
+        io::stdout(), 
+        "winning score (part 2): {:?}", 
+        players.iter().map(|p| p.points).max().unwrap()
+    )?;
+    Ok(())
+}
+
+fn play_game(players: &mut [Player], circle: &mut Circle, marbles: u32) {
+    let start = circle.max_marble() + 1;
+    let end = start + marbles;
+    for (player_id, value) in (0..players.len()).cycle().zip(start..end) {
+        circle.turn(&mut players[player_id], value);
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+struct Player {
+    points: u32,
+}
+
+struct Marble {
+    value: u32,
+    prev: usize,
+    next: usize,
+}
+
+struct Circle {
+    marbles: Vec<Marble>,
+    current: usize,
+}
+
+impl Circle {
+    fn new() -> Self {
+        let first = Marble { value: 0, prev: 0, next: 0 };
+        Self {
+            marbles: vec![first],
+            current: 0
+        }
+    }
+
+    fn turn(&mut self, player: &mut Player, value: u32) {
+        let marble_id = self.add_marble(value);
+        if value %23 !=  0 {
+            let insert_at = self.clockwise(1);
+            self.insert_after(marble_id, insert_at);
+            self.current = marble_id;
+        } else {
+            player.points += value;
+            let remove_id = self.counter_clockwise(7);
+            player.points += self.marbles[remove_id].value;
+            self.remove(remove_id);
+            self.current = self.counter_clockwise(6);
+        }
+    }
+
+    fn max_marble(&self) -> u32 {
+        (self.marbles.len() - 1) as u32
+    }
+
+    fn add_marble(&mut self, value: u32) -> usize {
+        let id = self.marbles.len();
+        self.marbles.push(Marble { value, prev: 0, next: 0});
+        id
+    }
+
+    fn insert_after(&mut self, to_insert: usize, after: usize) {
+        let old_next = self.marbles[after].next;
+        self.marbles[after].next = to_insert;
+        self.marbles[old_next].prev = to_insert;
+        self.marbles[to_insert].prev = after;
+        self.marbles[to_insert].next = old_next;
+    }
+
+    fn remove(&mut self, id: usize, ) {
+        let (prev, next) = (self.marbles[id].prev, self.marbles[id].next);
+        self.marbles[prev].next = next;
+        self.marbles[next].prev = prev;
+    }
+
+    fn clockwise(&mut self, mut i: usize) -> usize {
+        let mut id = self.current;
+        while i > 0 {
+            id = self.marbles[id].next;
+            i -= 1;
+        }
+        id
+    }
+
+    fn counter_clockwise(&mut self, mut i: usize) -> usize {
+        let mut id = self.current;
+        while i > 0 {
+            id = self.marbles[id].prev;
+            i -= 1;
+        }
+        id
+    }
+}
+
+impl fmt::Debug for Circle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut id = self.current;
+        loop {
+            let m = &self.marbles[id];
+            write!(f, "{}", m.value)?;
+            id = m.next;
+            if id == self.current {
+                break;
+            }
+        }
+        Ok(())
+    }
 }
