0d4770dafd6dc96545300cedb8dbc2d72e6368bd
livexia <istillalive@live.cn>
2021-03-25 10:29:26 +0800

    20210325 02 inoreder/dfs

---
+++ leetcode/leetcode_173/src/lib.rs
@@ -0,0 +1,98 @@
+use std::{cell::RefCell, rc::Rc};
+
+/**
+实现一个二叉搜索树迭代器。你将使用二叉搜索树的根节点初始化迭代器。
+
+调用 next() 将返回二叉搜索树中的下一个最小的数。
+
+示例：
+BSTIterator iterator = new BSTIterator(root);
+iterator.next();    // 返回 3
+iterator.next();    // 返回 7
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 9
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 15
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 20
+iterator.hasNext(); // 返回 false
+ 
+提示：
+next() 和 hasNext() 操作的时间复杂度是 O(1)，并使用 O(h) 内存，其中 h 是树的高度。
+你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 中至少存在一个下一个最小的数。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-search-tree-iterator
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+struct BSTIterator {
+    order: Vec<i32>
+}
+
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl BSTIterator {
+
+    fn new(root: Option<Rc<RefCell<TreeNode>>>) -> Self {
+        let mut order = vec![];
+        dfs(&root, &mut order);
+        BSTIterator { order }
+    }
+    
+    fn next(&mut self) -> i32 {
+        if let Some(val) = self.order.pop() {
+            val
+        } else {
+            panic!()
+        }
+    }
+    
+    fn has_next(&self) -> bool {
+        !self.order.is_empty()
+    }
+}
+
+fn dfs(node: &Option<Rc<RefCell<TreeNode>>>, result: &mut Vec<i32>){
+    if let Some(node) = node {
+        dfs(&node.borrow().right, result);
+        result.push(node.borrow().val);
+        dfs(&node.borrow().left, result);
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * let obj = BSTIterator::new(root);
+ * let ret_1: i32 = obj.next();
+ * let ret_2: bool = obj.has_next();
+ */
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
