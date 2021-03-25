7288ee3b34135f1214d211c04e4d78e3a27a2ee4
livexia <istillalive@live.cn>
2021-03-25 21:53:04 +0800

    update

--- practice/advent-of-code/aoc15/src/main.rs
+++ practice/advent-of-code/aoc15/src/main.rs
@@ -1,10 +1,14 @@
-use std::{fmt::write, io::{self, Read, Write}};
+#[cfg(test)]
+mod test;
+
+use std::io::{self, Read, Write};
 use std::error::Error;
 use std::result;
 use std::fmt;
 use std::str::FromStr;
 use std::cmp::Ordering;
 use std::collections::BTreeMap;
+use std::collections::HashSet;
 use std::iter::FromIterator;
 
 macro_rules! err {
@@ -29,6 +33,54 @@ struct Caves {
     grid: BTreeMap<Coordinate, Cell>,
     units: BTreeMap<Coordinate, Unit>,
     max: Coordinate,
+}
+
+impl Caves {
+    fn step(&mut self) {
+        todo!()
+    }
+
+    fn is_wall(&self, c: &Coordinate) -> bool {
+        if let Some(c) = self.grid.get(&c) {
+            c == &Cell::Wall
+        } else {
+            true
+        }
+    }
+
+    fn is_open(&self, c: &Coordinate) -> bool {
+        !self.is_wall(c)
+    }
+
+    fn near(&self, c: &Coordinate) -> Vec<Coordinate> {
+        let mut near = vec![];
+        let x = c.x;
+        let y = c.y;
+        if c.x < self.max.x {
+            near.push(Coordinate { x: x + 1, y});
+        }
+        if c.y < self.max.y {
+            near.push(Coordinate { x, y: y + 1 });
+        }
+        if c.y > 0 {
+            near.push(Coordinate { x, y: y - 1 });
+        }
+        if c.x > 0 {
+            near.push(Coordinate { x: x - 1, y});
+        }
+        near.into_iter().filter(|c| self.is_open(c)).collect()
+    }
+
+    fn any_targets(&self, c: &Coordinate) -> bool {
+        let u = &self.units[c];
+        for unit in self.units.values() {
+            if u.is_target(&unit) {
+                return true
+            }
+        }
+        false
+    }
+    
 }
 
 impl FromStr for Caves {
@@ -61,13 +113,17 @@ impl FromStr for Caves {
 
 impl fmt::Display for Caves {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut units = vec![];
         for (c, cell) in &self.grid {
             if let Some(ref unit) = self.units.get(c) {
                 write!(f, "{}", unit)?;
+                units.push(format!("{:?}", unit));
             } else {
                 write!(f, "{}", cell)?;
             }
             if c.x == self.max.x {
+                write!(f, "    {}", units.join(", "))?;
+                units.clear();
                 write!(f, "\n")?;
             }
         }
@@ -77,7 +133,8 @@ impl fmt::Display for Caves {
 
 
 
-#[derive(Clone, Copy, Debug)]
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Cell {
     Open,
     Wall,
@@ -111,6 +168,37 @@ struct Coordinate {
     y: usize
 }
 
+impl Coordinate {
+    fn is_adjacent(&self, other: &Coordinate) -> bool {
+        match self.distance(other) {
+            1 => true,
+            _ => false,
+        }
+    }
+
+    fn distance(&self, other: &Coordinate) -> usize {
+        let dx = (self.x as i32 - other.x as i32).abs() as usize;
+        let dy = (self.y as i32 - other.y as i32).abs() as usize;
+        dx + dy
+    }
+    
+    fn up(&self) -> Self {
+        Self { x: self.x - 1, y: self.y }
+    }
+    
+    fn down(&self) -> Self {
+        Self { x: self.x + 1, y: self.y }
+    }
+    
+    fn left(&self) -> Self {
+        Self { x: self.x, y: self.y - 1 }
+    }
+    
+    fn right(&self) -> Self {
+        Self { x: self.x, y: self.y + 1 }
+    }
+}
+
 impl Ord for Coordinate {
     fn cmp(&self, other: &Self) -> Ordering {
         self.partial_cmp(other).unwrap()
@@ -129,11 +217,34 @@ enum UnitKind {
     Goblin,
 }
 
-#[derive(Debug)]
+#[derive(Clone)]
 struct Unit {
     attack: i32,
     hp: i32,
     kind: UnitKind,
+}
+
+impl Unit {
+    fn is_target(&self, other: &Unit) -> bool {
+        use crate::UnitKind::*;
+        match (self.kind, other.kind) {
+            (Elf, Goblin) => true,
+            (Goblin, Elf) => true,
+            _ => false
+        }
+    }
+
+    fn attack(&mut self, other: &mut Unit) {
+        other.hp -= self.attack;
+    }
+
+    fn is_dead(&mut self) -> bool {
+        if self.hp <= 0 {
+            true
+        } else {
+            false
+        }
+    }
 }
 
 impl FromStr for Unit {
@@ -155,6 +266,15 @@ impl fmt::Display for Unit {
         match self.kind {
             UnitKind::Elf => write!(f, "E"),
             UnitKind::Goblin => write!(f, "G"),
+        }
+    }
+}
+
+impl fmt::Debug for Unit {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.kind {
+            UnitKind::Elf => write!(f, "E({})", self.hp),
+            UnitKind::Goblin => write!(f, "G({})", self.hp),
         }
     }
 }
