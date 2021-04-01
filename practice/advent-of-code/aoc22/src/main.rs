6c32bb109f7969de8387f80b659b4a822421dc8f
livexia <istillalive@live.cn>
2021-04-01 18:19:31 +0800

    aoc22 part2 copy

--- practice/advent-of-code/aoc22/src/main.rs
+++ practice/advent-of-code/aoc22/src/main.rs
@@ -1,7 +1,11 @@
 use std::io::{self, Read, Write};
 use std::error::Error;
+use std::fmt;
 use std::result;
 use std::str::FromStr;
+use std::cmp::Reverse;
+use std::collections::HashMap;
+use std::collections::BinaryHeap;
 use std::collections::BTreeMap;
 
 macro_rules! err {
@@ -14,10 +18,10 @@ fn main() -> Result<()>{
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
     
-    let mut cave: Cave = Cave::new(&input)?;
+    let cave: Cave = Cave::new(&input)?;
     
     part1(&cave)?;
-
+    part2(&cave)?;
 
     Ok(())
 }
@@ -42,13 +46,26 @@ fn part1(cave: &Cave) -> Result<()> {
     Ok(())
 }
 
+fn part2(cave: &Cave) -> Result<()> {
+    writeln!(io::stdout(), "part2 answer: {}", cave.shortest_time()?)?;
+    Ok(())
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
+enum Tool {
+    Torch,
+    ClimbingGear,
+    Neither,
+}
+
 #[derive(Debug)]
 struct Cave {
-    depth: u32,
+    depth: i32,
     target: Coordinate,
+    bound: Coordinate,
     regions: BTreeMap<Coordinate, Region>,
-    erosion: BTreeMap<Coordinate, u32>,
-    geologic: BTreeMap<Coordinate, u32>,
+    erosion: BTreeMap<Coordinate, i32>,
+    geologic: BTreeMap<Coordinate, i32>,
 }
 
 impl Cave {
@@ -60,10 +77,13 @@ impl Cave {
                 cave = Cave {
                     depth: depth.parse()?,
                     target: target.parse()?,
+                    bound: target.parse()?,
                     regions: BTreeMap::new(),
                     erosion: BTreeMap::new(),
                     geologic: BTreeMap::new(),
                 };
+                cave.bound.x *= 2;
+                cave.bound.y *= 2;
                 cave.init()?;
                 return Ok(cave);
             } else {
@@ -79,18 +99,16 @@ impl Cave {
         self.erosion.insert(Coordinate::new(0, 0), (0 + self.depth) % 20183);
         self.geologic.insert(self.target, 0);
         self.erosion.insert(self.target, (0 + self.depth) % 20183);
-        let max_x = self.target.x + 5;
-        let max_y = self.target.y + 5;
-        for x in 0..max_x {
+        for x in 0..=self.bound.x {
             self.calc_geologic(&Coordinate::new(x, 0))?;
             self.calc_erosion(&Coordinate::new(x, 0))?;
         }
-        for y in 0..max_y {
+        for y in 0..=self.bound.y {
             self.calc_geologic(&Coordinate::new(0, y))?;
             self.calc_erosion(&Coordinate::new(0, y))?;
         }
-        for x in 1..max_x {
-            for y in 1..max_y {
+        for x in 1..=self.bound.x {
+            for y in 1..=self.bound.y {
                 self.calc_geologic(&Coordinate::new(x, y))?;
                 self.calc_erosion(&Coordinate::new(x, y))?;
             }
@@ -128,6 +146,73 @@ impl Cave {
         self.erosion.insert(c.clone(), erosion);
         Ok(())
     }
+
+    fn string(&self, me: &Coordinate) -> String {
+        let mut result = String::new();
+        for y in 0..self.target.y+10 {
+            for x in 0..self.target.x+10 {
+                let cur = Coordinate::new(x, y);
+                if self.target == cur {
+                    result.push_str("T");
+                } else if x == 0 && y == 0 {
+                    result.push_str("M");
+                } else if me == &cur {
+                    result.push_str("X");
+                } else {
+                    result.push_str(&format!("{}", self.regions.get(&cur).unwrap()));
+                }
+            }
+            result.push_str("\n");
+        }
+        result
+    }
+
+    fn shortest_time(&self) -> Result<usize> {
+        type Time = usize;
+        type PriorityQueue = BinaryHeap<Reverse<(Time, Coordinate, Tool)>>;
+        let mut queue: PriorityQueue = BinaryHeap::new();
+        let mut best: HashMap<(Coordinate, Tool), Time> = HashMap::new();
+
+        queue.push(Reverse((0, Coordinate::new(0, 0), Tool::Torch)));
+        while let Some(Reverse((time, c, tool))) = queue.pop() {
+            if best.contains_key(&(c, tool)) && best[&(c, tool)] <= time {
+                continue;
+            }
+            best.insert((c, tool), time);
+            if c == self.target && tool == Tool::Torch {
+                return Ok(time)
+            }
+            
+            for &e in &[Tool::Torch, Tool::ClimbingGear, Tool::Neither] {
+                if self.regions.get(&c).unwrap().can_equip(e) {
+                    queue.push(Reverse((time + 7, c, e)))
+                }
+            }
+            for &(x, y) in &[(0, -1), (1, 0), (0, 1), (-1, 0)] {
+                if (x < 0 && c.x == 0) || (y <= 0 && c.y == 0) {
+                    continue;
+                }
+
+                let x = (c.x as i64 + x) as i32;
+                let y = (c.y as i64 + y) as i32;
+                if x > self.bound.x || y > self.bound.y {
+                    continue;
+                }
+                if self.regions.get(&Coordinate::new(x, y)).unwrap().can_equip(tool) {
+                    let neighbor = Coordinate::new(x, y);
+                    queue.push(Reverse((time + 1, neighbor, tool)));
+                }
+            }
+        }
+        err!("could not find a path to {:?}", self.target)
+    }
+}
+
+impl fmt::Display for Cave {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.string(&Coordinate::new(0, 0)))?;
+        Ok(())
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -137,14 +222,39 @@ enum Region {
     Wet
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+impl Region {
+    fn can_equip(&self, tool: Tool) -> bool {
+        use crate::Region::*;
+        use crate::Tool::*;
+
+        match (self, tool) {
+            (Rocky, Torch) | (Rocky, ClimbingGear) => true,
+            (Wet, ClimbingGear) | (Wet, Neither) => true,
+            (Narrow, Torch) | (Narrow, Neither) => true,
+            _ => false
+        }
+    }
+}
+
+impl fmt::Display for Region {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Region::Rocky => write!(f, ".")?,
+            Region::Narrow => write!(f, "|")?,
+            Region::Wet => write!(f, "=")?,
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Coordinate {
-    x: u32,
-    y: u32,
+    x: i32,
+    y: i32,
 }
 
 impl Coordinate {
-    fn new(x: u32, y: u32) -> Self {
+    fn new(x: i32, y: i32) -> Self {
         Self{ x, y }
     }
 }
