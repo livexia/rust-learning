92f109f5d13208e04775e0dabe615bfbca8256d1
livexia <istillalive@live.cn>
2021-04-05 09:30:16 +0800

    20210405 01 array

---
+++ leetcode/leetcode_88/src/lib.rs
@@ -0,0 +1,46 @@
+/**
+给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+
+示例 1：
+输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+输出：[1,2,2,3,5,6]
+
+示例 2：
+输入：nums1 = [1], m = 1, nums2 = [], n = 0
+输出：[1]
+ 
+提示：
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[i] <= 109
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+    let mut m = m as usize;
+    let mut n = n as usize;
+    while n > 0 {
+        if m > 0 && nums1[m - 1] > nums2[n - 1] {
+            nums1[m + n - 1] = nums1[m - 1];
+            m -= 1;
+        } else {
+            nums1[m + n - 1] = nums2[n - 1];
+            n -= 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
