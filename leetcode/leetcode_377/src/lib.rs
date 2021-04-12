9834361546a8dbc247702c7b8e1bb83219a132d8
livexia <istillalive@live.cn>
2021-04-12 10:37:54 +0800

    20210412 02 dp

---
+++ leetcode/leetcode_377/src/lib.rs
@@ -0,0 +1,63 @@
+/**
+给你一个由 不同 整数组成的数组 nums ，和一个目标整数 target 。请你从 nums 中找出并返回总和为 target 的元素组合的个数。
+
+题目数据保证答案符合 32 位整数范围。
+
+ 
+
+示例 1：
+
+输入：nums = [1,2,3], target = 4
+输出：7
+解释：
+所有可能的组合为：
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+请注意，顺序不同的序列被视作不同的组合。
+示例 2：
+
+输入：nums = [9], target = 3
+输出：0
+ 
+
+提示：
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 1000
+nums 中的所有元素 互不相同
+1 <= target <= 1000
+ 
+
+进阶：如果给定的数组中含有负数会发生什么？问题会产生何种变化？如果允许负数出现，需要向题目中添加哪些限制条件？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/combination-sum-iv
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+pub fn combination_sum4(nums: Vec<i32>, target: i32) -> i32 {
+    if nums.is_empty() { return 0 }
+    let mut dp = vec![0; target as usize + 1];
+    dp[0] = 1;
+    for i in 1..=target as usize {
+        for &num in &nums {
+            if i >= num as usize {
+                dp[i] += dp[i - num as usize]
+            }
+        }
+    }
+    dp[target as usize] as i32
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
