c0a09df02141e542fbd3efcb828d9ed52bade1da
livexia <istillalive@live.cn>
2022-01-10 19:05:42 +0800

    init tokio

---
+++ Tokio/my-redis/src/main.rs
@@ -0,0 +1,19 @@
+use std::result;
+
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Open a connection to the mini-redis-server address.
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // Set the key "hello" with value "world"
+    client.set("hello", "world".into()).await?;
+
+    // Get key "hello"
+    let result = client.get("hello").await?;
+
+    println!("got value frome the server; result={:?}", result);
+
+    Ok(())
+}
