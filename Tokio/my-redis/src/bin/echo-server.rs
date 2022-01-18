5a30cbb6ab50091d9ebeeaf8ec90f90095d80d66
livexia <istillalive@live.cn>
2022-01-18 13:12:48 +0800

    io manual copying

---
+++ Tokio/my-redis/src/bin/echo-server.rs
@@ -0,0 +1,39 @@
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    // us nc 127.0.0.1 6142 to make a simple client
+    let listener = TcpListener::bind("127.0.0.1:6142").await?;
+
+    loop {
+        let (mut socket, addr) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
+
+            loop {
+                match socket.read(&mut buf).await {
+                    // Return value of `Ok(0)` signifies that the remote has
+                    // closed
+                    // see https://tokio.rs/tokio/tutorial/io#:~:text=Handling-,EOF,-When%20the%20read
+                    Ok(0) => return,
+                    Ok(n) => {
+                        println!("FROM: {:?}, GOT: {:?}", addr, &buf[..n]);
+                        // Copy the data back to socket
+                        if socket.write_all(&buf[..n]).await.is_err() {
+                            // Unexpected socket error. There isn't much we can
+                            // do here so just stop processing.
+                            return;
+                        }
+                    }
+                    Err(_) => {
+                        // Unexpected socket error. There isn't much we can do
+                        // here so just stop processing.
+                        return;
+                    }
+                }
+            }
+        });
+    }
+}
