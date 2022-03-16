44d0f960cb4c8928dba46d32126e039217872c31
livexia <istillalive@live.cn>
2022-03-16 14:24:46 +0800

    update delimiter type to generic D

--- Crust of Rust/Lifetime Annotations/strsplit/src/lib.rs
+++ Crust of Rust/Lifetime Annotations/strsplit/src/lib.rs
@@ -1,26 +1,32 @@
-
-pub struct StrSplit<'haystack, 'delimiter> {
+pub struct StrSplit<'haystack, D> {
     remainder: Option<&'haystack str>,
-    delimiter: &'delimiter str,
+    delimiter: D,
 }
 
-impl<'haystack, 'delimiter> StrSplit<'haystack, 'delimiter> {
-    pub fn new(haystack: &'haystack str, delimiter: &'delimiter str) -> Self {
+impl<'haystack, D> StrSplit<'haystack, D> {
+    pub fn new(haystack: &'haystack str, delimiter: D) -> Self {
         Self {
             remainder: Some(haystack),
-            delimiter
+            delimiter,
         }
     }
 }
 
-impl<'haystack, 'delimiter> Iterator for StrSplit<'haystack, 'delimiter> {
+pub trait Delimiter {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)>;
+}
+
+impl<'haystack, D> Iterator for StrSplit<'haystack, D>
+where
+    D: Delimiter,
+{
     type Item = &'haystack str;
 
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(ref mut remainder) = self.remainder {
-            if let Some(start) = remainder.find(self.delimiter) {
+            if let Some((start, end)) = self.delimiter.find_next(remainder) {
                 let until_delimiter = &remainder[..start];
-                self.remainder = Some(&remainder[(start + self.delimiter.len())..]);
+                self.remainder = Some(&remainder[end..]);
                 Some(until_delimiter)
             } else {
                 self.remainder.take()
@@ -31,20 +37,46 @@ impl<'haystack, 'delimiter> Iterator for StrSplit<'haystack, 'delimiter> {
     }
 }
 
-
-pub fn until_char(s: &str, c: char) -> &str {
-    let delimiter = format!("{}", c);
-    StrSplit::new(s, &delimiter).next().unwrap()
+impl Delimiter for char {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)> {
+        s.char_indices()
+            .position(|(_, c)| &c == self)
+            // .map(|start| (start, start + 1))
+            .map(|start| (start, start + self.len_utf8()))   // use len_utf_8() to make sure use right char length
+    }
 }
 
+impl Delimiter for &str {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)> {
+        s.find(self).map(|start| (start, start + self.len()))
+    }
+}
+
+pub fn until_char(s: &str, c: char) -> &str {
+    StrSplit::new(s, c).next().unwrap()
+}
+
+pub fn until_string(s: &str, p: String) -> &str {
+    StrSplit::new(s, &p[..]).next().unwrap()
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::{StrSplit, until_char};
+    use crate::{until_char, until_string, StrSplit};
 
     #[test]
     fn until_char_test() {
         assert_eq!(until_char("hello, world", 'o'), "hell");
+    }
+
+    #[test]
+    fn until_utf8char_test() {
+        assert_eq!(until_char("hello😀, world", '😀'), "hello");
+    }
+
+    #[test]
+    fn until_string_test() {
+        assert_eq!(until_string("hello, world", 'o'.to_string()), "hell");
     }
 
     #[test]
