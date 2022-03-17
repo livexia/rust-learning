be363c1c7173525903386e9b693635803195085e
livexia <istillalive@live.cn>
2022-03-17 14:26:43 +0800

    merge empty, single and multipy together

--- Crust of Rust/Declarative Macros/vecmac/src/lib.rs
+++ Crust of Rust/Declarative Macros/vecmac/src/lib.rs
@@ -1,11 +1,9 @@
 #[macro_export]
 macro_rules! avec {
-    () => {
-        Vec::new()
-    };
-    ($elem: expr) => {{
+    ($($elem: expr), *) => {{
+        #[allow(unused_mut)]
         let mut v = Vec::new();
-        v.push($elem);
+        $(v.push($elem);)*
         v
     }};
 }
@@ -22,4 +20,13 @@ fn single() {
     assert!(!v.is_empty());
     assert_eq!(v.len(), 1);
     assert_eq!(v[0], 5);
+}
+
+#[test]
+fn double() {
+    let v: Vec<u32> = avec![5, 6];
+    assert!(!v.is_empty());
+    assert_eq!(v.len(), 2);
+    assert_eq!(v[0], 5);
+    assert_eq!(v[1], 6);
 }
