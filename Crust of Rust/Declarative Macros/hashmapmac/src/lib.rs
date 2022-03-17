70d871e052ed44b0b2500dacdda022635b5db110
livexia <istillalive@live.cn>
2022-03-17 15:27:25 +0800

    merge single and empty and multiple with one pattern

--- Crust of Rust/Declarative Macros/hashmapmac/src/lib.rs
+++ Crust of Rust/Declarative Macros/hashmapmac/src/lib.rs
@@ -1,11 +1,9 @@
 #[macro_export]
 macro_rules! hashmap {
-    () => {
-        ::std::collections::HashMap::new()
-    };
-    ($key: expr => $value: expr) => {{
+    ($($key: expr => $value: expr), *) => {{
+        #[allow(unused_mut)]
         let mut map = ::std::collections::HashMap::new();
-        map.insert($key, $value);
+        $(map.insert($key, $value);)*
         map
     }};
 }
@@ -20,8 +18,21 @@ fn empty_hashmap() {
 #[test]
 fn sigle_hashmap() {
     use std::collections::HashMap;
-    let map: HashMap<u32, u32> = hashmap! {1 => 2};
+    let map: HashMap<u32, u32> = hashmap! { 1 => 2 };
     assert!(!map.is_empty());
     assert_eq!(map.len(), 1);
     assert_eq!(map[&1], 2);
+}
+
+#[test]
+fn double_hashmap() {
+    use std::collections::HashMap;
+    let map: HashMap<u32, char> = hashmap! {
+        1 => 'a',
+        2 => 'b'
+    };
+    assert!(!map.is_empty());
+    assert_eq!(map.len(), 2);
+    assert_eq!(map[&1], 'a');
+    assert_eq!(map[&2], 'b');
 }
