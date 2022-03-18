6a3ad1b62c01e1ff601769466cf767a96b85a52a
livexia <istillalive@live.cn>
2022-03-18 12:17:22 +0800

    finish flatten

--- Crust of Rust/Iterators/iterators/src/lib.rs
+++ Crust of Rust/Iterators/iterators/src/lib.rs
@@ -1,4 +1,4 @@
-fn flatten<I>(iter: I) -> Flatten<I::IntoIter>
+pub fn flatten<I>(iter: I) -> Flatten<I::IntoIter>
 where
     I: IntoIterator,
     I::Item: IntoIterator,
@@ -11,7 +11,8 @@ where
     O: Iterator,
     O::Item: IntoIterator,
 {
-    iter: O,
+    outer_iter: O,
+    inner_iter: Option<<O::Item as IntoIterator>::IntoIter>,
 }
 
 impl<O> Flatten<O>
@@ -20,7 +21,10 @@ where
     O::Item: IntoIterator,
 {
     pub fn new(iter: O) -> Self {
-        Flatten { iter }
+        Flatten {
+            outer_iter: iter,
+            inner_iter: None,
+        }
     }
 }
 
@@ -29,10 +33,19 @@ where
     O: Iterator,
     O::Item: IntoIterator,
 {
-    type Item = O;
+    type Item = <O::Item as IntoIterator>::Item;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        loop {
+            if let Some(ref mut inner_iter) = self.inner_iter {
+                if let Some(inner_item) = inner_iter.next() {
+                    return Some(inner_item);
+                }
+                self.inner_iter = None;
+            }
+            self.inner_iter = Some(self.outer_iter.next()?.into_iter());
+            // self.next() // use recursion or loop
+        }
     }
 }
 
@@ -41,12 +54,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(flatten(::std::iter::empty::<Vec<()>>()).count(), 0)
+    fn empty() {
+        assert_eq!(flatten(::std::iter::empty::<Vec<()>>()).count(), 0);
+    }
+
+    #[test]
+    fn once() {
+        assert_eq!(flatten(::std::iter::once(vec!['1'])).count(), 1);
+    }
+
+    #[test]
+    fn two() {
+        assert_eq!(flatten(::std::iter::once(vec!['1', '2'])).count(), 2);
     }
 
     #[test]
     fn deep() {
-        assert_eq!(flatten(vec![vec![1, 2, 3]]).count(), 3)
+        assert_eq!(flatten(vec![vec![1, 2, 3]]).count(), 3);
+    }
+    #[test]
+    fn wide() {
+        assert_eq!(flatten(vec![vec![1, 2, 3], vec![4, 5, 6]]).count(), 6);
     }
 }
