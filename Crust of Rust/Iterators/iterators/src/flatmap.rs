4ac3c644d019102c95cb40e2b83a276f80b782ec
livexia <istillalive@live.cn>
2022-03-18 14:04:58 +0800

    impl FlatMapExt to replace function

--- Crust of Rust/Iterators/iterators/src/flatmap.rs
+++ Crust of Rust/Iterators/iterators/src/flatmap.rs
@@ -1,3 +1,23 @@
+pub trait FlatMapExt: Iterator + Sized {
+    fn our_flat_map<F, U>(self, f: F) -> FlatMap<Self, F, U>
+    where
+        F: FnMut(Self::Item) -> U,
+        U: IntoIterator;
+}
+
+impl<T> FlatMapExt for T
+where
+    T: Iterator,
+{
+    fn our_flat_map<F, U>(self, f: F) -> FlatMap<Self, F, U>
+    where
+        F: FnMut(Self::Item) -> U,
+        U: IntoIterator,
+    {
+        FlatMap::new(self, f)
+    }
+}
+
 pub fn flat_map<I, F, U>(iter: I, f: F) -> FlatMap<I, F, U>
 where
     I: Iterator,
@@ -119,6 +139,35 @@ mod flatmap_test {
             x.into_iter().rev().collect::<Vec<i32>>()
         })
         .collect();
+        assert_eq!(sub_sum, vec![3, 2, 1, 6, 5, 4]);
+    }
+
+    #[test]
+    fn two_deep_ext() {
+        assert_eq!(
+            vec![vec![1, 2], vec![3, 4]]
+                .into_iter()
+                .our_flat_map(|x| x.into_iter().map(|i| i + 1))
+                .collect::<Vec<_>>(),
+            vec![2, 3, 4, 5]
+        );
+    }
+
+    #[test]
+    fn merge_word_ext() {
+        let merged: String = vec!["Hello", "World"]
+            .into_iter()
+            .our_flat_map(|x| x.chars())
+            .collect();
+        assert_eq!(&merged, "HelloWorld");
+    }
+
+    #[test]
+    fn rev_inner_ext() {
+        let sub_sum: Vec<i32> = vec![vec![1, 2, 3], vec![4, 5, 6]]
+            .into_iter()
+            .our_flat_map(|x| x.into_iter().rev().collect::<Vec<i32>>())
+            .collect();
         assert_eq!(sub_sum, vec![3, 2, 1, 6, 5, 4]);
     }
 }
