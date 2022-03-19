e94ecb5bbafe92c6e46894b33291868e5b50cdc8
livexia <istillalive@live.cn>
2022-03-19 17:37:07 +0800

    first draft for RefCell

--- Crust of Rust/Smart Pointers and Interior Mutability/pointers/src/refcell.rs
+++ Crust of Rust/Smart Pointers and Interior Mutability/pointers/src/refcell.rs
@@ -1,17 +1,36 @@
+use std::cell::UnsafeCell;
+
 pub struct RefCell<T> {
-    value: T,
+    value: UnsafeCell<T>,
 }
 
 impl<T> RefCell<T> {
     pub fn new(value: T) -> Self {
-        Self { value }
+        Self {
+            value: UnsafeCell::new(value),
+        }
     }
 
     pub fn borrow(&self) -> &T {
-        &self.value
+        unsafe { &*self.value.get() }
     }
 
     pub fn borrow_mut(&self) -> &mut T {
-        &mut self.value
+        unsafe { &mut *self.value.get() }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn panicked() {
+        use std::cell::RefCell;
+        let c = RefCell::new(42);
+
+        let m = c.borrow();
+        let b = c.borrow_mut();
+        // thread 'refcell::tests::it_works' panicked at 'already borrowed: BorrowMutError'
     }
 }
