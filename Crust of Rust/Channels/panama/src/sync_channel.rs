5b855ec85dd4d9289e5ac6eeca1db6cbab4d884d
livexia <istillalive@live.cn>
2022-03-21 00:02:22 +0800

    finish sync_channel

--- Crust of Rust/Channels/panama/src/sync_channel.rs
+++ Crust of Rust/Channels/panama/src/sync_channel.rs
@@ -1,5 +1,3 @@
-pub mod sync_channel;
-
 use std::collections::VecDeque;
 use std::sync::Arc;
 use std::sync::Condvar;
@@ -17,22 +15,23 @@ use std::sync::Mutex;
 //  - Rendezvous channels: Synchronous with capacity = 0. Used for thread synchronization.
 //  - Oneshot channels: Any capacity. In practice, only one call to send().
 
-pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+pub fn sync_channel<T>(bound: usize) -> (AsyncSender<T>, Receiver<T>) {
     let inner = Inner {
-        queue: VecDeque::default(),
+        queue: VecDeque::with_capacity(bound + 1),
         counter: 1,
+        bound,
     };
     let shared = Arc::new(Shared {
         inner: Mutex::new(inner),
-        availability: Condvar::new(),
+        recv_availability: Condvar::new(),
+        send_availability: Condvar::new(),
     });
     (
-        Sender {
+        AsyncSender {
             shared: Arc::clone(&shared), // 注意使用 Arc::clone 而不是 .clone
         },
         Receiver {
             shared: Arc::clone(&shared),
-            buffer: VecDeque::default(), // add buffer for recv optimization
         },
     )
 }
@@ -42,29 +41,46 @@ pub struct Inner<T> {
     // so there is another warpper for the queue and counter
     queue: VecDeque<T>,
     counter: usize,
+    bound: usize,
 }
 
 pub struct Shared<T> {
     inner: Mutex<Inner<T>>,
-    availability: Condvar, // Condvar cna not be in Muttex, so need another wrapper for the queue
+    recv_availability: Condvar, // Condvar cna not be in Muttex, so need another wrapper for the queue
+    send_availability: Condvar, // Condvar cna not be in Muttex, so need another wrapper for the queue
 }
 
-pub struct Sender<T> {
+pub struct AsyncSender<T> {
     shared: Arc<Shared<T>>,
 }
 
-impl<T> Sender<T> {
+impl<T> AsyncSender<T> {
     pub fn send(&self, value: T) {
         // use &self instead of &mut self, because shared use Arc<Mutex<_>> interior mutability give by the Mutex
         let mut shared = self.shared.inner.lock().unwrap();
-        shared.queue.push_back(value);
+        loop {
+            if shared.bound == 0 && shared.queue.len() == 0 {
+                shared.queue.push_back(value);
+                self.shared.recv_availability.notify_one();
+                let _ = self.shared.send_availability.wait(shared).unwrap();
+                return;
+            };
+            if shared.queue.len() < shared.bound {
+                shared.queue.push_back(value);
 
-        self.shared.availability.notify_one();
-        // after send notify the recv there is data on the queue, recvier can be wake up.
+                self.shared.recv_availability.notify_one();
+                return;
+                // after send notify the recv there is data on the queue, recvier can be wake up.
+            } else {
+                shared = self.shared.send_availability.wait(shared).unwrap();
+                // when there is no more room for the sender,
+                // block send, wait until some receiver recv the data
+            }
+        }
     }
 }
 
-impl<T> Clone for Sender<T> {
+impl<T> Clone for AsyncSender<T> {
     // implement Clone istead of using #[derive(Clone)]
     // #[derive(Clone)] require that the T is Clone, but we don't want T to be Clone
     fn clone(&self) -> Self {
@@ -79,7 +95,7 @@ impl<T> Clone for Sender<T> {
     }
 }
 
-impl<T> Drop for Sender<T> {
+impl<T> Drop for AsyncSender<T> {
     // now we drop the sender, we need to subtract the counter by one
     fn drop(&mut self) {
         // now we also need acquire the lock to modify the counter
@@ -90,14 +106,13 @@ impl<T> Drop for Sender<T> {
         drop(inner);
         if was_last {
             // when there is no sender, notify receiver should be wake to return None
-            self.shared.availability.notify_one();
+            self.shared.recv_availability.notify_one();
         }
     }
 }
 
 pub struct Receiver<T> {
     shared: Arc<Shared<T>>,
-    buffer: VecDeque<T>,
     // add buffer to receiver,
     // because there is only one receiver, so it is ok to put buffer outside the Mutex
     // when there is data one the buffer, just pop from the buffer, no need to acquire the lock
@@ -106,14 +121,9 @@ pub struct Receiver<T> {
 }
 
 impl<T> Receiver<T> {
-    pub fn recv(&mut self) -> Option<T> {
+    pub fn recv(&self) -> Option<T> {
         // we want when there is no data on the queue, recv is blocked
         // use &self instead of &mut self, because shared use Arc<Mutex<_>> interior mutability give by the Mutexs
-        // because of the buffer, we need &mut self, to quick swap the buffer and the queue.
-        if let Some(t) = self.buffer.pop_front() {
-            // when there is data one the buffer, just pop from the buffer, no need to acquire the lock
-            return Some(t);
-        }
 
         // when there is data on tge queue, return the first value
         // if there is no data, drop the lock, then rerun the loop.
@@ -121,7 +131,9 @@ impl<T> Receiver<T> {
         loop {
             match shared.queue.pop_front() {
                 Some(t) => {
-                    ::std::mem::swap(&mut self.buffer, &mut shared.queue);
+                    // eprintln!("inside lock");
+                    // notify_all sender when the data were taken out
+                    self.shared.send_availability.notify_all();
                     return Some(t);
                 }
                 None if shared.counter == 0 => return None,
@@ -133,7 +145,7 @@ impl<T> Receiver<T> {
                     // and when there is more date on the queue, we need to wake up the receiver.
                     // we use Condvar see https://doc.rust-lang.org/std/sync/struct.Condvar.html
 
-                    shared = self.shared.availability.wait(shared).unwrap();
+                    shared = self.shared.recv_availability.wait(shared).unwrap();
                     // unwrap to ignore possible thread poison
                     // when there is no sender, this will be hang,
                     // so we need a counter to keep track of the number of senders.
@@ -151,14 +163,14 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let (tx, mut rx) = channel();
+        let (tx, rx) = sync_channel(1);
         tx.send(42);
         assert_eq!(rx.recv(), Some(42));
     }
 
     #[test]
     fn two_one() {
-        let (tx, mut rx) = channel();
+        let (tx, rx) = sync_channel(2);
         tx.send(42);
         assert_eq!(rx.recv(), Some(42));
         let tx1 = tx.clone();
@@ -169,7 +181,7 @@ mod tests {
     #[test]
     fn no_sender() {
         // expect not hang
-        let (tx, mut rx) = channel::<()>();
+        let (tx, rx) = sync_channel::<()>(1);
         drop(tx);
         let x = rx.recv();
         assert_eq!(x, None)
@@ -180,7 +192,7 @@ mod tests {
         // this will hang because tx not drop
         // always will remain atleast one sender
         use std::thread;
-        let (tx, mut rx) = channel();
+        let (tx, rx) = sync_channel(1);
         let tx1 = tx.clone();
         let tx2 = tx.clone();
 
@@ -193,5 +205,27 @@ mod tests {
         let x3 = rx.recv();
         assert_eq!(85, x1.unwrap() + x2.unwrap());
         assert_eq!(x3, None);
+    }
+
+    #[test]
+    fn sync_channel_rendezvous_test() {
+        // this will hang because sync_channel cannot handle size 0 channel
+        // see https://doc.rust-lang.org/std/sync/mpsc/struct.SyncSender.html#method.send
+        use std::thread;
+
+        // Create a rendezvous sync_channel with buffer size 0
+        let (sync_sender, receiver) = sync_channel(0);
+
+        thread::spawn(move || {
+            println!("sending message...");
+            sync_sender.send(1);
+            // Thread is now blocked until the message is received
+
+            println!("...message received!");
+        });
+
+        let msg = receiver.recv().unwrap();
+        println!("msg: {}", msg);
+        assert_eq!(1, msg);
     }
 }
