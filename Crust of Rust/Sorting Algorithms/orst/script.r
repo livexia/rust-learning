ec2eafadff9fa3deeb587ad2178abfc3cd576e94
livexia <istillalive@live.cn>
2022-03-21 21:19:44 +0800

    upload r script

---
+++ Crust of Rust/Sorting Algorithms/orst/script.r
@@ -0,0 +1,8 @@
+t <- read.table('values.dat', header=TRUE)
+library(ggplot2)
+# to plot # comparisons
+ggplot(t, aes(n, comparisions, colour = algorithm)) + geom_point() + scale_y_log10()
+# to plot runtime
+ggplot(t, aes(n, time, colour = algorithm)) + geom_point() + scale_y_log10(
+# add line
+ggplot(t, aes(n, time, colour = algorithm)) + geom_point() + geom_line() + scale_y_log10()
