d43675e790bab8df07cf9392b2661d034888312a
livexia <istillalive@live.cn>
2023-01-05 13:00:05 +0800

    finish day 9 part1

---
+++ practice/advent-of-code-2020/aoc09/src/main.rs
@@ -0,0 +1,77 @@
+use std::collections::HashSet;
+use std::error::Error;
+use std::io::{self, Read, Write};
+use std::time::Instant;
+
+macro_rules! err {
+    ($($tt:tt)*) => { Err(Box::<dyn Error>::from(format!($($tt)*))) }
+}
+
+type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
+type Int = u64;
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    let input: Vec<Int> = input.lines().map(|l| l.trim().parse().unwrap()).collect();
+
+    part1(&input, 25)?;
+    // part2()?;
+    Ok(())
+}
+
+fn part1(input: &[Int], length: usize) -> Result<Int> {
+    let start = Instant::now();
+
+    let mut result = None;
+    for left in 0..input.len() - length - 1 {
+        if !follow_the_rule(&input[left..left + length], input[left + length]) {
+            result = Some(input[left + length]);
+            break;
+        }
+    }
+    if result.is_none() {
+        return err!("Not found");
+    }
+
+    writeln!(io::stdout(), "Part 1: {}", result.unwrap())?;
+    writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
+    Ok(result.unwrap())
+}
+
+fn follow_the_rule(preamble: &[Int], num: Int) -> bool {
+    let mut visited = HashSet::new();
+    for &p in preamble {
+        if num >= p && visited.contains(&(num - p)) {
+            return true;
+        }
+        visited.insert(p);
+    }
+    false
+}
+
+#[test]
+fn example_input() {
+    let input = "35
+    20
+    15
+    25
+    47
+    40
+    62
+    55
+    65
+    95
+    102
+    117
+    150
+    182
+    127
+    219
+    299
+    277
+    309
+    576";
+    let input: Vec<Int> = input.lines().map(|l| l.trim().parse().unwrap()).collect();
+    assert_eq!(part1(&input, 5).unwrap(), 127);
+}
