8d0f434920ed907a323cc64f2089487c366b2de4
livexia <istillalive@live.cn>
2023-01-11 17:36:33 +0800

    failed day 19 part 2

--- practice/advent-of-code-2020/aoc19/src/main.rs
+++ practice/advent-of-code-2020/aoc19/src/main.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
 use std::error::Error;
 use std::io::{self, Read, Write};
 use std::time::Instant;
 
+#[allow(unused_macros)]
 macro_rules! err {
     ($($tt:tt)*) => { Err(Box::<dyn Error>::from(format!($($tt)*))) }
 }
@@ -15,13 +15,14 @@ fn main() -> Result<()> {
     let (rules, messages) = parse_input(&input)?;
 
     part1(&rules, &messages)?;
-    // part2()?;
+    part2(&rules, &messages)?;
     Ok(())
 }
-fn part1(rules: &[Rule], messages: &[Vec<char>]) -> Result<usize> {
+
+fn part1(rules: &[Option<Rule>], messages: &[Vec<char>]) -> Result<usize> {
     let start = Instant::now();
 
-    let r = &rules[0];
+    let r = rules[0].as_ref().unwrap();
     let result = messages
         .iter()
         .filter(|m| {
@@ -30,29 +31,65 @@ fn part1(rules: &[Rule], messages: &[Vec<char>]) -> Result<usize> {
         })
         .count();
 
-    writeln!(io::stdout(), "Part 1 {result}")?;
+    writeln!(io::stdout(), "Part 1: {result}")?;
     writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
     Ok(result)
 }
 
-#[derive(Debug)]
+fn part2(rules: &[Option<Rule>], messages: &[Vec<char>]) -> Result<usize> {
+    let start = Instant::now();
+
+    let rules = replace_rules(rules);
+    let r = rules[0].as_ref().unwrap();
+    let result = messages
+        .iter()
+        .filter(|m| {
+            let (matched, length) = r.match_msg(m, &rules);
+            if matched && length == m.len() {
+                println!("{}", String::from_iter(m.iter()))
+            }
+            matched && length == m.len()
+        })
+        .count();
+
+    writeln!(io::stdout(), "Part 2: {result}")?;
+    writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
+    Ok(result)
+}
+
+fn replace_rules(rules: &[Option<Rule>]) -> Vec<Option<Rule>> {
+    let mut rules = rules.to_vec();
+    rules[8] = Some(Rule::Sub(vec![vec![42], vec![42, 8]]));
+    rules[11] = Some(Rule::Sub(vec![vec![42, 31], vec![42, 11, 31]]));
+    rules
+}
+
+#[derive(Debug, Clone)]
 enum Rule {
     Sub(Vec<Vec<usize>>),
     Single(char),
 }
 
 impl Rule {
-    fn match_msg(&self, msg: &[char], rules: &[Rule]) -> (bool, usize) {
+    fn match_msg(&self, msg: &[char], rules: &[Option<Rule>]) -> (bool, usize) {
         match self {
             Rule::Sub(subs) => {
                 'subs: for sub in subs {
                     let mut start = 0;
-                    for &r in sub {
-                        let (matched, checked_length) = rules[r].match_msg(&msg[start..], rules);
-                        if matched {
-                            start += checked_length;
+                    for (i, &r) in sub.iter().enumerate() {
+                        if let Some(r) = &rules[r] {
+                            if start >= msg.len() {
+                                return (true, start);
+                            }
+                            let (matched, checked_length) = r.match_msg(&msg[start..], rules);
+
+                            if matched {
+                                start += checked_length;
+                            } else {
+                                continue 'subs;
+                            }
                         } else {
-                            continue 'subs;
+                            unreachable!()
                         }
                     }
                     return (true, start);
@@ -64,7 +101,7 @@ impl Rule {
     }
 }
 
-fn parse_input(input: &str) -> Result<(Vec<Rule>, Vec<Vec<char>>)> {
+fn parse_input(input: &str) -> Result<(Vec<Option<Rule>>, Vec<Vec<char>>)> {
     let mut rules = vec![];
     let mut messages = vec![];
 
@@ -85,7 +122,7 @@ fn parse_input(input: &str) -> Result<(Vec<Rule>, Vec<Vec<char>>)> {
                 }
                 let content: Vec<&str> = content.split(' ').collect();
                 if content.len() == 1 && content[0].starts_with('"') {
-                    rules[id] = Some(Rule::Single(content[0].chars().skip(1).next().unwrap()))
+                    rules[id] = Some(Rule::Single(content[0].chars().nth(1).unwrap()))
                 } else {
                     let mut sub = vec![];
                     let mut subs = vec![];
@@ -107,7 +144,6 @@ fn parse_input(input: &str) -> Result<(Vec<Rule>, Vec<Vec<char>>)> {
             messages.push(line.trim().chars().collect())
         }
     }
-    let rules = rules.into_iter().map(|r| r.unwrap()).collect();
     Ok((rules, messages))
 }
 
@@ -139,4 +175,61 @@ fn example_input() {
     aaaabbb";
     let (rules, messages) = parse_input(&input).unwrap();
     assert_eq!(part1(&rules, &messages).unwrap(), 2);
+
+    let input = "42: 9 14 | 10 1
+    9: 14 27 | 1 26
+    10: 23 14 | 28 1
+    1: \"a\"
+    11: 42 31
+    5: 1 14 | 15 1
+    19: 14 1 | 14 14
+    12: 24 14 | 19 1
+    16: 15 1 | 14 14
+    31: 14 17 | 1 13
+    6: 14 14 | 1 14
+    2: 1 24 | 14 4
+    0: 8 11
+    13: 14 3 | 1 12
+    15: 1 | 14
+    17: 14 2 | 1 7
+    23: 25 1 | 22 14
+    28: 16 1
+    4: 1 1
+    20: 14 14 | 1 15
+    3: 5 14 | 16 1
+    27: 1 6 | 14 18
+    14: \"b\"
+    21: 14 1 | 1 14
+    25: 1 1 | 1 14
+    22: 14 14
+    8: 42
+    26: 14 22 | 1 20
+    18: 15 15
+    7: 14 5 | 1 21
+    24: 14 1
+    
+    abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+    bbabbbbaabaabba
+    babbbbaabbbbbabbbbbbaabaaabaaa
+    aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+    bbbbbbbaaaabbbbaaabbabaaa
+    bbbababbbbaaaaaaaabbababaaababaabab
+    ababaaaaaabaaab
+    ababaaaaabbbaba
+    baabbaaaabbaaaababbaababb
+    abbbbabbbbaaaababbbbbbaaaababb
+    aaaaabbaabaaaaababaa
+    aaaabbaaaabbaaa
+    aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+    babaaabbbaaabaababbaabababaaab
+    aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba";
+    let (rules, messages) = parse_input(&input).unwrap();
+    assert_eq!(part1(&rules, &messages).unwrap(), 3);
+
+    // let rules = replace_rules(&rules);
+    // let r = rules[0].as_ref().unwrap();
+    // let result = r.match_msg(&"aaaabbaaaabbaaa".chars().collect::<Vec<_>>(), &rules);
+    // println!("{:?}", result);
+
+    assert_eq!(part2(&rules, &messages).unwrap(), 12);
 }
