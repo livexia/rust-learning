6996637a866407fa73934d741cc33224f2d04a17
livexia <istillalive@live.cn>
2023-01-13 11:51:06 +0800

    finish day 22 part 2 with useless cache

--- practice/advent-of-code-2020/aoc22/src/main.rs
+++ practice/advent-of-code-2020/aoc22/src/main.rs
@@ -1,5 +1,8 @@
+use std::collections::hash_map::DefaultHasher;
 use std::collections::VecDeque;
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
+use std::hash::{Hash, Hasher};
 use std::io::{self, Read, Write};
 use std::time::Instant;
 
@@ -17,7 +20,7 @@ fn main() -> Result<()> {
     let (player_one, player_two) = parse_input(&input)?;
 
     part1(player_one.clone(), player_two.clone())?;
-    // part2()?;
+    part2(player_one, player_two)?;
     Ok(())
 }
 
@@ -25,29 +28,92 @@ fn part1(mut player_one: Deck, mut player_two: Deck) -> Result<usize> {
     let start = Instant::now();
 
     while !player_one.is_empty() && !player_two.is_empty() {
-        let a = player_one.pop_back().unwrap();
-        let b = player_two.pop_back().unwrap();
-        if a > b {
-            player_one.push_front(a);
-            player_one.push_front(b);
-        } else {
-            player_two.push_front(b);
-            player_two.push_front(a);
-        }
+        let a = player_one.pop_front().unwrap();
+        let b = player_two.pop_front().unwrap();
+        let player_one_win = a > b;
+        wins(&mut player_one, &mut player_two, player_one_win, a, b);
     }
 
-    let result = player_one
-        .iter()
-        .enumerate()
-        .fold(0, |sum, (i, n)| sum + (i + 1) * n)
-        + player_two
-            .iter()
-            .enumerate()
-            .fold(0, |sum, (i, n)| sum + (i + 1) * n);
+    let result = score(&player_one) + score(&player_two);
 
     writeln!(io::stdout(), "Part 1: {result}")?;
     writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
     Ok(result)
+}
+
+fn part2(mut player_one: Deck, mut player_two: Deck) -> Result<usize> {
+    let start = Instant::now();
+
+    let result = if game(&mut player_one, &mut player_two, &mut HashMap::new()) {
+        score(&player_one)
+    } else {
+        score(&player_two)
+    };
+
+    writeln!(io::stdout(), "Part 1: {result}")?;
+    writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
+    Ok(result)
+}
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
+
+fn game(
+    player_one: &mut Deck,
+    player_two: &mut Deck,
+    cache: &mut HashMap<(u64, u64), bool>,
+) -> bool {
+    let h1 = calculate_hash(&player_one);
+    let h2 = calculate_hash(&player_two);
+    if let Some(r) = cache.get(&(h1, h2)) {
+        return *r;
+    }
+    let mut player_one_decks = HashSet::new();
+    let mut player_two_decks = HashSet::new();
+    while !player_one.is_empty() && !player_two.is_empty() {
+        let h1 = calculate_hash(&player_one);
+        let h2 = calculate_hash(&player_two);
+        if !player_one_decks.insert(h1) || !player_two_decks.insert(h2) {
+            return true;
+        }
+        let a = player_one.pop_front().unwrap();
+        let b = player_two.pop_front().unwrap();
+        if a > player_one.len() || b > player_two.len() {
+            let player_one_win = a > b;
+            wins(player_one, player_two, player_one_win, a, b);
+        } else {
+            let mut player_one_copy = player_one.clone();
+            player_one_copy.truncate(a);
+            let mut player_two_copy = player_two.clone();
+            player_two_copy.truncate(b);
+            let player_one_win = game(&mut player_one_copy, &mut player_two_copy, cache);
+            wins(player_one, player_two, player_one_win, a, b);
+        }
+    }
+    let r = !player_one.is_empty();
+    cache.insert((h1, h2), r);
+    r
+}
+
+fn wins(player_one: &mut Deck, player_two: &mut Deck, player_one_win: bool, a: usize, b: usize) {
+    if player_one_win {
+        player_one.push_back(a);
+        player_one.push_back(b);
+    } else {
+        player_two.push_back(b);
+        player_two.push_back(a);
+    }
+}
+
+fn score(player: &Deck) -> usize {
+    player
+        .iter()
+        .rev()
+        .enumerate()
+        .fold(0, |sum, (i, n)| sum + (i + 1) * n)
 }
 
 fn parse_input(input: &str) -> Result<(Deck, Deck)> {
@@ -61,7 +127,7 @@ fn parse_input(input: &str) -> Result<(Deck, Deck)> {
         } else if line.trim().starts_with("Player 2") {
             cur = &mut players.1;
         } else {
-            cur.push_front(line.trim().parse()?);
+            cur.push_back(line.trim().parse()?);
         }
     }
     Ok(players)
@@ -85,4 +151,19 @@ fn example_input() {
 
     let (player_one, player_two) = parse_input(input).unwrap();
     assert_eq!(part1(player_one.clone(), player_two.clone()).unwrap(), 306);
+    assert_eq!(part2(player_one, player_two).unwrap(), 291);
+}
+#[test]
+fn loop_input() {
+    let input = "Player 1:
+    43
+    19
+    
+    Player 2:
+    2
+    29
+    14";
+
+    let (player_one, player_two) = parse_input(input).unwrap();
+    part2(player_one, player_two).unwrap();
 }
