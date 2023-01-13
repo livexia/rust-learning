2ee5b988dd705d5cb4096f8d9f10b511e1d1ca3f
livexia <istillalive@live.cn>
2023-01-13 20:50:07 +0800

    use "linked list" instead of array

--- practice/advent-of-code-2020/aoc23/src/main.rs
+++ practice/advent-of-code-2020/aoc23/src/main.rs
@@ -12,18 +12,18 @@ type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-    let cups = parse_input(&input);
+    let (cups, cur, end) = parse_input(&input);
 
-    part1(&cups)?;
-    part2(&cups)?;
+    part1(&cups, cur)?;
+    part2(&cups, cur, end)?;
     Ok(())
 }
 
-fn part1(cups: &[u32]) -> Result<String> {
+fn part1(cups: &[usize], cur: usize) -> Result<String> {
     let start = Instant::now();
 
     let mut cups = cups.to_owned();
-    nth_moves(&mut cups, 100);
+    nth_moves(&mut cups, cur, 100);
     let result = order_after_one(&cups);
 
     writeln!(io::stdout(), "Part 1: {result}")?;
@@ -31,96 +31,105 @@ fn part1(cups: &[u32]) -> Result<String> {
     Ok(result)
 }
 
-fn part2(cups: &[u32]) -> Result<u64> {
+fn part2(cups: &[usize], cur: usize, end: usize) -> Result<usize> {
     let start = Instant::now();
 
-    let total_cups = 20;
+    let length = 1000000;
+    let times = length * 10;
 
-    let mut cups: Vec<u32> = cups.iter().map(|&n| n as u32).collect();
-    let max = cups.iter().max().unwrap() + 1;
-    cups.extend(max..=total_cups);
-    dbg!(cups.len());
-    nth_moves(&mut cups, total_cups * 10);
+    let mut cups = cups.to_owned();
+    expand_cups(&mut cups, cur, end, length);
 
-    let one_pos = cups.iter().position(|&n| n == 1).unwrap();
-    let r1 = cups[(one_pos + 1) % total_cups as usize];
-    let r2 = cups[(one_pos + 2) % total_cups as usize];
-    dbg!(r1, r2);
-    let result = r1 as u64 * r2 as u64;
+    nth_moves(&mut cups, cur, times);
+
+    let cup1 = cups[1];
+    let cup2 = cups[cup1];
+    let result = cup1 * cup2;
 
     writeln!(io::stdout(), "Part 2: {result}")?;
     writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
     Ok(result)
 }
 
-fn nth_moves(cups: &mut Vec<u32>, times: u32) {
-    let &min = cups.iter().min().unwrap();
-    let &max = cups.iter().max().unwrap();
-    for t in 0..times {
-        if t % 10000 == 0 {
-            println!("t: {}/100", t / 10000);
-        }
-        move_cup(cups, min, max);
-        println!("{:?}", cups);
-    }
-}
-
-fn move_cup(cups: &mut Vec<u32>, min: u32, max: u32) {
-    let cur = cups.remove(0);
-    let pick_up = [cups.remove(0), cups.remove(0), cups.remove(0)];
-    let dest = dest(cur, &pick_up, min, max);
-    dbg!(cur, dest);
-    let dest_pos = cups.iter().position(|&n| n == dest).unwrap() + 1;
-    for (offset, n) in pick_up.into_iter().enumerate() {
-        cups.insert(dest_pos + offset, n);
-    }
-    cups.push(cur);
-}
-
-fn dest(cur: u32, pick_up: &[u32; 3], min: u32, max: u32) -> u32 {
+fn nth_moves(cups: &mut [usize], cur: usize, times: usize) {
+    let min = 1;
+    let max = cups.len() - 1;
     let mut cur = cur;
-    loop {
-        cur = if cur - 1 < min { max } else { cur - 1 };
-        if !pick_up.contains(&cur) {
-            return cur;
-        }
+    for _ in 0..times {
+        cur = move_cup(cups, cur, min, max);
     }
 }
 
-fn order_after_one(cups: &[u32]) -> String {
+fn move_cup(cups: &mut [usize], cur: usize, min: usize, max: usize) -> usize {
+    let pick_up = [cups[cur], cups[cups[cur]], cups[cups[cups[cur]]]];
+    let dest = dest(cur, &pick_up, min, max);
+    cups[cur] = cups[pick_up[2]]; // cur -> p[0] -> p[1] -> p[2] -> next
+    cups[pick_up[2]] = cups[dest]; // p[2] -> dest -> next
+    cups[dest] = pick_up[0]; // dest -> p[0]
+    cups[cur]
+}
+
+fn dest(start: usize, pick_up: &[usize; 3], min: usize, max: usize) -> usize {
+    let mut cur = start;
+    while cur == start || pick_up[0] == cur || pick_up[1] == cur || pick_up[2] == cur {
+        cur = if cur - 1 < min { max } else { cur - 1 };
+    }
+    cur
+}
+
+fn order_after_one(cups: &[usize]) -> String {
+    let mut cur = cups[1];
     let mut s = String::new();
-    let mut found = 0;
-    for &n in cups.iter().cycle() {
-        if n == 1 {
-            found += 1;
-            if found == 2 {
-                break;
-            }
-        } else if found == 1 {
-            s.push((n as u8 + b'0') as char)
-        }
+    while cur != 1 {
+        s.push_str(&format!("{}", cur));
+        cur = cups[cur];
     }
     s
 }
 
-fn parse_input(input: &str) -> Vec<u32> {
-    input
+fn parse_input(input: &str) -> (Vec<usize>, usize, usize) {
+    let mut cups: Vec<usize> = (0..=input.trim().len()).collect();
+    let input: Vec<_> = input
         .trim()
         .chars()
-        .map(|c| (c as u8 - b'0') as u32)
-        .collect()
+        .map(|c| (c as u8 - b'0') as usize)
+        .collect();
+    for i in 0..input.len() - 1 {
+        cups[input[i]] = input[i + 1];
+    }
+    cups[input[input.len() - 1]] = input[0];
+    (cups, input[0], input[input.len() - 1])
+}
+
+fn expand_cups(cups: &mut Vec<usize>, start: usize, end: usize, length: usize) {
+    let max = cups.len();
+    cups.reserve_exact(length + 1);
+    cups[end] = max;
+    cups.extend(max + 1..=length + 1);
+    cups[length] = start;
+}
+
+#[allow(dead_code)]
+fn pretty_cups(cups: &[usize], start: usize) -> Vec<usize> {
+    let mut result = vec![start];
+    let mut cur = cups[start];
+    while cur != start {
+        result.push(cur);
+        cur = cups[cur];
+    }
+    result
 }
 
 #[test]
 fn example_input() {
     let input = "389125467";
-    let cups = parse_input(&input);
+    let (cups, cur, end) = parse_input(&input);
 
     let mut test = cups.clone();
-    nth_moves(&mut test, 10);
+    nth_moves(&mut test, cur, 10);
     assert_eq!("92658374", order_after_one(&test));
 
-    assert_eq!("67384529", part1(&cups).unwrap());
+    assert_eq!("67384529", part1(&cups, cur).unwrap());
 
-    assert_eq!(149245887792, part2(&cups).unwrap());
+    assert_eq!(149245887792, part2(&cups, cur, end).unwrap());
 }
