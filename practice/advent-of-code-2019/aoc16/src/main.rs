5be2902b1ecb4426750e06b18278f0413c64047c
livexia <istillalive@live.cn>
2023-01-22 23:41:56 +0800

    optimization fft

--- practice/advent-of-code-2019/aoc16/src/main.rs
+++ practice/advent-of-code-2019/aoc16/src/main.rs
@@ -1,6 +1,5 @@
 use std::error::Error;
 use std::io::{self, Read, Write};
-use std::iter::repeat;
 use std::time::Instant;
 
 macro_rules! err {
@@ -9,6 +8,7 @@ macro_rules! err {
 
 type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 
+#[allow(dead_code)]
 const BASE: [i32; 4] = [0, 1, 0, -1];
 
 fn main() -> Result<()> {
@@ -16,7 +16,7 @@ fn main() -> Result<()> {
     io::stdin().read_to_string(&mut input)?;
 
     part1(&input, 100)?;
-    // part2()?;
+    part2(&input)?;
     Ok(())
 }
 
@@ -26,6 +26,18 @@ fn part1(input: &str, phase_count: usize) -> Result<String> {
     let result = get_eight_digit_message(input, phase_count, 0)?;
 
     writeln!(io::stdout(), "Part 1: {result}")?;
+    writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
+    Ok(result)
+}
+
+fn part2(input: &str) -> Result<String> {
+    let start = Instant::now();
+
+    let input = input.repeat(10000);
+    let offset = input.chars().take(7).collect::<String>().parse()?;
+    let result = get_eight_digit_message(&input, 100, offset)?;
+
+    writeln!(io::stdout(), "Part 2: {result}")?;
     writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
     Ok(result)
 }
@@ -42,17 +54,13 @@ fn fft(input: &[i32]) -> Vec<i32> {
     let mut v = vec![];
     for i in 0..input.len() {
         v.push(ones_digit(
-            input
-                .iter()
-                .zip(
-                    BASE.into_iter()
-                        .flat_map(|p| repeat(p).take(i + 1))
-                        .cycle()
-                        .skip(1),
-                )
-                .map(|(n, p)| n * p)
-                .sum(),
-        ))
+            input[i..]
+                .chunks(i + 1)
+                .map(|it| it.iter().sum::<i32>())
+                .step_by(2)
+                .enumerate()
+                .fold(0, |sum, (i, w)| if i % 2 == 0 { sum + w } else { sum - w }),
+        ));
     }
     v
 }
@@ -90,5 +98,10 @@ fn example_input() {
     assert_eq!(
         part1("80871224585914546619083218645595", 100).unwrap(),
         "24176176".to_string()
+    );
+
+    assert_eq!(
+        part2("03036732577212944063491565474664").unwrap(),
+        "84462026".to_string()
     );
 }
