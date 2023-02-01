063924b8526834a34b7714c0e528e59b3d52d09b
livexia <istillalive@live.cn>
2023-02-01 19:29:05 +0800

    prepare for part 2 day 20

--- practice/advent-of-code-2019/aoc20/src/main.rs
+++ practice/advent-of-code-2019/aoc20/src/main.rs
@@ -17,7 +17,7 @@ fn main() -> Result<()> {
     let (portals, grid) = parse_grid(&input);
 
     part1(&portals, &grid)?;
-    // part2()?;
+    part2(&portals, &grid)?;
     Ok(())
 }
 
@@ -26,11 +26,67 @@ fn part1(portals: &HashMap<String, Vec<Coord>>, grid: &[Vec<Kind>]) -> Result<us
 
     let src = portals.get("AA").unwrap()[0];
     let dest = portals.get("ZZ").unwrap()[0];
-    let result = bfs(src, dest, grid, portals).unwrap();
+    let result = bfs(src, dest, grid, portals)?;
 
     writeln!(io::stdout(), "Part 1: {result}")?;
     writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
     Ok(result)
+}
+
+fn part2(portals: &HashMap<String, Vec<Coord>>, grid: &[Vec<Kind>]) -> Result<usize> {
+    let start = Instant::now();
+
+    let src = portals.get("AA").unwrap()[0];
+    let dest = portals.get("ZZ").unwrap()[0];
+    let result = bfs_with_level(src, dest, grid, portals)?;
+
+    writeln!(io::stdout(), "Part 2: {result}")?;
+    writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
+    Ok(result)
+}
+
+fn bfs_with_level(
+    src: Coord,
+    dest: Coord,
+    grid: &[Vec<Kind>],
+    portals: &HashMap<String, Vec<Coord>>,
+) -> Result<usize> {
+    let mut queue = VecDeque::new();
+    let mut visited = HashSet::new();
+    queue.push_back((src, 0, 0));
+    while let Some((cur, depth, level)) = queue.pop_front() {
+        if cur == dest && level == 0 {
+            return Ok(depth);
+        }
+        if visited.insert((cur, level)) {
+            let (x, y) = cur;
+            for next in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)] {
+                match &grid[next.0][next.1] {
+                    Kind::Open => queue.push_back((next, depth + 1, level)),
+                    Kind::Portal(s) => {
+                        for &other in portals.get(s).unwrap() {
+                            if other != next {
+                                let new_level =
+                                    next_level(next.0, next.1, level, grid.len(), grid[0].len());
+
+                                queue.push_back((other, depth + 2, new_level))
+                            }
+                        }
+                    }
+                    _ => continue,
+                }
+            }
+        }
+    }
+    err!("can not found path from AA to ZZ")
+}
+
+fn next_level(x: usize, y: usize, level: usize, height: usize, width: usize) -> usize {
+    if x == 2 || x == height - 3 || y == 2 || y == width - 3 {
+        level.saturating_sub(1)
+    } else {
+        level + 1
+    }
 }
 
 fn bfs(
@@ -38,13 +94,13 @@ fn bfs(
     dest: Coord,
     grid: &[Vec<Kind>],
     portals: &HashMap<String, Vec<Coord>>,
-) -> Option<usize> {
+) -> Result<usize> {
     let mut queue = VecDeque::new();
     let mut visited = HashSet::new();
     queue.push_back((src, 0));
     while let Some((cur, depth)) = queue.pop_front() {
         if cur == dest {
-            return Some(depth);
+            return Ok(depth);
         }
         if visited.insert(cur) {
             let (x, y) = cur;
@@ -65,7 +121,8 @@ fn bfs(
             }
         }
     }
-    None
+
+    err!("can not found path from AA to ZZ")
 }
 
 #[derive(Debug, Clone)]
@@ -187,4 +244,45 @@ YN......#               VT..#....QG
            U   P   P               ";
     let (portals, grid) = parse_grid(input);
     assert_eq!(part1(&portals, &grid).unwrap(), 58);
+
+    let input = "
+             Z L X W       C                 
+             Z P Q B       K                 
+  ###########.#.#.#.#######.###############  
+  #...#.......#.#.......#.#.......#.#.#...#  
+  ###.#.#.#.#.#.#.#.###.#.#.#######.#.#.###  
+  #.#...#.#.#...#.#.#...#...#...#.#.......#  
+  #.###.#######.###.###.#.###.###.#.#######  
+  #...#.......#.#...#...#.............#...#  
+  #.#########.#######.#.#######.#######.###  
+  #...#.#    F       R I       Z    #.#.#.#  
+  #.###.#    D       E C       H    #.#.#.#  
+  #.#...#                           #...#.#  
+  #.###.#                           #.###.#  
+  #.#....OA                       WB..#.#..ZH
+  #.###.#                           #.#.#.#  
+CJ......#                           #.....#  
+  #######                           #######  
+  #.#....CK                         #......IC
+  #.###.#                           #.###.#  
+  #.....#                           #...#.#  
+  ###.###                           #.#.#.#  
+XF....#.#                         RF..#.#.#  
+  #####.#                           #######  
+  #......CJ                       NM..#...#  
+  ###.#.#                           #.###.#  
+RE....#.#                           #......RF
+  ###.###        X   X       L      #.#.#.#  
+  #.....#        F   Q       P      #.#.#.#  
+  ###.###########.###.#######.#########.###  
+  #.....#...#.....#.......#...#.....#.#...#  
+  #####.#.###.#######.#######.###.###.#.#.#  
+  #.......#.......#.#.#.#.#...#...#...#.#.#  
+  #####.###.#####.#.#.#.#.###.###.#.###.###  
+  #.......#.....#.#...#...............#...#  
+  #############.#.#.###.###################  
+               A O F   N                     
+               A A D   M                     ";
+    let (portals, grid) = parse_grid(input);
+    assert_eq!(part2(&portals, &grid).unwrap(), 396);
 }
