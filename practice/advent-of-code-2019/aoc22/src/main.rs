6b6980f3ee1adda6a94e6cda8877586833dfd0c1
livexia <istillalive@live.cn>
2023-02-04 11:08:43 +0800

    finish day 22 part 1

--- practice/advent-of-code-2019/aoc22/src/main.rs
+++ practice/advent-of-code-2019/aoc22/src/main.rs
@@ -15,89 +15,95 @@ fn main() -> Result<()> {
     let ts = paesr_input(&input)?;
 
     part1(&ts)?;
-    // part2()?;
+    part2(&ts)?;
     Ok(())
 }
 
 fn part1(techniques: &[Technique]) -> Result<usize> {
     let start = Instant::now();
 
-    let (deck, first) = shuffle(10007, techniques);
-    println!("{}", show_deck(&deck, first));
+    let deck = shuffle(10007, techniques, 1);
 
-    let result = 0;
+    let result = deck[2019];
 
     writeln!(io::stdout(), "Part 1: {result}")?;
     writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
     Ok(result)
 }
 
-fn shuffle(length: usize, techniques: &[Technique]) -> (Vec<usize>, usize) {
-    let mut deck: Vec<_> = (1..length).collect(); // index: card number, value: next card
-    deck.push(0);
+fn part2(techniques: &[Technique]) -> Result<usize> {
+    let start = Instant::now();
 
-    let mut first = 0;
-    for t in techniques {
-        match t {
-            Technique::DealNew => {
-                let mut cur = first;
-                let mut next = deck[cur];
+    // let deck = shuffle(119315717514047, techniques, 101741582076661);
 
-                while next != first {
-                    let temp = deck[next];
-                    deck[next] = cur;
-                    cur = next;
-                    next = temp;
+    // let v = vec![0; 119315717514047];
+
+    let deck = shuffle(10009, techniques, 101741582076661);
+
+    let result = 0;
+
+    writeln!(io::stdout(), "Part 2: {result}")?;
+    writeln!(io::stdout(), "> Time elapsed is: {:?}", start.elapsed())?;
+    Ok(result)
+}
+
+fn shuffle(length: usize, techniques: &[Technique], times: usize) -> Vec<usize> {
+    let mut deck: Vec<_> = (0..length).collect(); // index: card number, value: card index
+
+    let mut visited = std::collections::HashMap::new();
+    let mut loop_size = usize::MAX;
+
+    for _i in 0..times {
+        for t in techniques {
+            match t {
+                Technique::DealNew => {
+                    for i in deck.iter_mut() {
+                        *i = length - 1 - *i;
+                    }
                 }
-                deck[first] = cur;
-                first = cur;
-            }
-            Technique::Cut(n) => {
-                first = (first
-                    + if n < &0 {
+                Technique::Cut(n) => {
+                    let index = if n < &0 {
                         length - n.abs() as usize
                     } else {
                         *n as usize
-                    })
-                    % length;
-            }
-            Technique::DealIncrement(n) => {
-                println!("n:{n}, {}", show_deck(&deck, first));
-                assert!(n > &0);
-                let mut new_decks = deck.clone();
-                let mut cur = first;
-                for i in 0..length {
-                    let temp = new_decks[cur];
-                    let x = (i + 1 + length - *n as usize) % length;
-
-                    println!("{} jump {} to {}", cur, x, get_n_card(x, &new_decks, first));
-                    new_decks[cur] = get_n_card(x % length, &new_decks, first) + 1;
-                    cur = temp;
+                    };
+                    for i in 0..length {
+                        if deck[i] < index {
+                            deck[i] += length - index
+                        } else {
+                            deck[i] -= index
+                        }
+                    }
                 }
-                deck = new_decks;
-                println!("new {}", show_deck(&deck, first));
+                Technique::DealIncrement(n) => {
+                    for i in 0..length {
+                        deck[i] = (deck[i] * *n as usize) % length;
+                    }
+                }
             }
         }
+        // println!("{:?}", deck);
+        let v = deck.iter().position(|i| i == &5752).unwrap();
+        if let Some(prev) = visited.get(&v) {
+            println!("{}: {} {}, {}", v, prev, _i, _i - prev);
+            loop_size = _i - prev;
+            break;
+        }
+        visited.insert(v, _i);
     }
-    (deck, first)
+    dbg!(visited.iter().find(|(_, &i)| i == times % loop_size));
+
+    deck
 }
 
-fn get_n_card(n: usize, deck: &[usize], first: usize) -> usize {
-    let mut cur = first;
-    for _ in 0..n {
-        cur = deck[cur];
-    }
-    cur
-}
-
-fn show_deck(deck: &[usize], first: usize) -> String {
+fn show_deck(deck: &[usize]) -> String {
     let mut s = String::new();
-    let mut cur = first;
-    s.push_str(&format!("{cur} "));
-    while deck[cur] != first {
-        s.push_str(&format!("{} ", deck[cur]));
-        cur = deck[cur];
+    let mut deck: Vec<(usize, &usize)> = deck.iter().enumerate().collect();
+    deck.sort_by_key(|(_, &i)| i);
+    for (i, _) in deck {
+        s.push_str(&format!("{i} "));
     }
+
     s.trim().to_string()
 }
 
@@ -133,63 +139,63 @@ fn paesr_input(input: &str) -> Result<Vec<Technique>> {
 
 #[test]
 fn example_input() {
-    // let input = "deal with increment 7
-    // deal into new stack
-    // deal into new stack";
-    // let ts = paesr_input(input).unwrap();
-    // let (deck, first) = shuffle(10, &ts);
-    // assert_eq!(&show_deck(&deck, first), "0 3 6 9 2 5 8 1 4 7");
+    let input = "deal with increment 7
+    deal into new stack
+    deal into new stack";
+    let ts = paesr_input(input).unwrap();
+    let deck = shuffle(10, &ts, 1);
+    assert_eq!(&show_deck(&deck), "0 3 6 9 2 5 8 1 4 7");
 
-    // let input = "deal into new stack";
-    // let ts = paesr_input(input).unwrap();
-    // let (deck, first) = shuffle(10, &ts);
-    // assert_eq!(&show_deck(&deck, first), "9 8 7 6 5 4 3 2 1 0");
+    let input = "deal into new stack";
+    let ts = paesr_input(input).unwrap();
+    let deck = shuffle(10, &ts, 1);
+    assert_eq!(&show_deck(&deck), "9 8 7 6 5 4 3 2 1 0");
 
-    // let input = "cut 3";
-    // let ts = paesr_input(input).unwrap();
-    // let (deck, first) = shuffle(10, &ts);
-    // assert_eq!(&show_deck(&deck, first), "3 4 5 6 7 8 9 0 1 2");
+    let input = "cut 3";
+    let ts = paesr_input(input).unwrap();
+    let deck = shuffle(10, &ts, 1);
+    assert_eq!(&show_deck(&deck), "3 4 5 6 7 8 9 0 1 2");
 
-    // let input = "cut -4";
-    // let ts = paesr_input(input).unwrap();
-    // let (deck, first) = shuffle(10, &ts);
-    // assert_eq!(&show_deck(&deck, first), "6 7 8 9 0 1 2 3 4 5");
+    let input = "cut -4";
+    let ts = paesr_input(input).unwrap();
+    let deck = shuffle(10, &ts, 1);
+    assert_eq!(&show_deck(&deck), "6 7 8 9 0 1 2 3 4 5");
 
-    // let input = "deal with increment 3";
-    // let ts = paesr_input(input).unwrap();
-    // let (deck, first) = shuffle(10, &ts);
-    // assert_eq!(&show_deck(&deck, first), "0 7 4 1 8 5 2 9 6 3");
+    let input = "deal with increment 3";
+    let ts = paesr_input(input).unwrap();
+    let deck = shuffle(10, &ts, 1);
+    assert_eq!(&show_deck(&deck), "0 7 4 1 8 5 2 9 6 3");
 
     let input = "deal with increment 9";
     let ts = paesr_input(input).unwrap();
-    let (deck, first) = shuffle(10, &ts);
-    assert_eq!(&show_deck(&deck, first), "0 9 8 7 6 5 4 3 2 1");
+    let deck = shuffle(10, &ts, 1);
+    assert_eq!(&show_deck(&deck), "0 9 8 7 6 5 4 3 2 1");
 
-    // let input = "cut 6
-    // deal with increment 7
-    // deal into new stack";
-    // let ts = paesr_input(input).unwrap();
-    // let (deck, first) = shuffle(10, &ts);
-    // assert_eq!(&show_deck(&deck, first), "3 0 7 4 1 8 5 2 9 6");
+    let input = "cut 6
+    deal with increment 7
+    deal into new stack";
+    let ts = paesr_input(input).unwrap();
+    let deck = shuffle(10, &ts, 1);
+    assert_eq!(&show_deck(&deck), "3 0 7 4 1 8 5 2 9 6");
 
     let input = "deal with increment 7
     deal with increment 9
     cut -2";
     let ts = paesr_input(input).unwrap();
-    let (deck, first) = shuffle(10, &ts);
-    assert_eq!(&show_deck(&deck, first), "6 3 0 7 4 1 8 5 2 9");
+    let deck = shuffle(10, &ts, 1);
+    assert_eq!(&show_deck(&deck), "6 3 0 7 4 1 8 5 2 9");
 
-    // let input = "deal into new stack
-    // cut -2
-    // deal with increment 7
-    // cut 8
-    // cut -4
-    // deal with increment 7
-    // cut 3
-    // deal with increment 9
-    // deal with increment 3
-    // cut -1";
-    // let ts = paesr_input(input).unwrap();
-    // let (deck, first) = shuffle(10, &ts);
-    // assert_eq!(&show_deck(&deck, first), "9 2 5 8 1 4 7 0 3 6");
+    let input = "deal into new stack
+    cut -2
+    deal with increment 7
+    cut 8
+    cut -4
+    deal with increment 7
+    cut 3
+    deal with increment 9
+    deal with increment 3
+    cut -1";
+    let ts = paesr_input(input).unwrap();
+    let deck = shuffle(10, &ts, 1);
+    assert_eq!(&show_deck(&deck), "9 2 5 8 1 4 7 0 3 6");
 }
