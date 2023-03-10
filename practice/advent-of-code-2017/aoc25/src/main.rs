cf58a25815c571383baff41c94f6e194b9520555
livexia <istillalive@live.cn>
2023-03-10 21:26:01 +0800

    todo: use nom to parse input for day 25

--- practice/advent-of-code-2017/aoc25/src/main.rs
+++ practice/advent-of-code-2017/aoc25/src/main.rs
@@ -1,3 +1,10 @@
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take, take_till, take_until},
+    character::is_alphabetic,
+    sequence::preceded,
+    Finish, IResult,
+};
 use std::error::Error;
 use std::io::{self, Read, Write};
 use std::time::Instant;
@@ -7,35 +14,62 @@ macro_rules! err {
 }
 
 type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
-type State = ((u8, i32, usize), (u8, i32, usize));
+type Rule = (u8, i32, usize);
+type State = (Rule, Rule);
 
-fn parse_input(input: &str) -> (usize, usize, Vec<State>) {
-    let mut begin: usize = 0;
-    let mut steps: usize = 0;
-    let mut input = input.split("\n\n");
-    if let Some(rows) = input.next() {
-        let mut rows = rows.lines();
-        if let Some(first) = rows.next() {
-            if let Some(first) = first.strip_prefix("Begin in state ") {
-                begin = (first.bytes().next().unwrap() - b'A') as usize;
-            }
-        }
-        if let Some(second) = rows.next() {
-            if let Some(second) = second.strip_prefix("Perform a diagnostic checksum after ") {
-                steps = second.split(' ').next().unwrap().parse().unwrap();
-            }
-        }
-    }
+fn state_to_id(input: &str) -> usize {
+    (input.bytes().next().unwrap() - b'A') as usize
+}
+
+fn parse_rule(input: &str) -> IResult<&str, Rule> {
+    let (input, write_value) = preceded(
+        take_until("Write the value "),
+        preceded(take_till(|c: char| c.is_numeric()), take(1usize)),
+    )(input)?;
+    let (input, move_dir) = preceded(
+        take_until("Move one slot to the "),
+        preceded(tag("Move one slot to the "), take(1usize)),
+    )(input)?;
+    let move_dir = if move_dir == "r" { 1 } else { -1 };
+    let (input, next_state) = preceded(
+        take_until("Continue with state "),
+        preceded(tag("Continue with state "), take(1usize)),
+    )(input)?;
+    println!("{:?} {:?} {:?}", write_value, move_dir, next_state);
+    Ok((
+        input,
+        (
+            write_value.parse().unwrap(),
+            move_dir,
+            state_to_id(next_state),
+        ),
+    ))
+}
+
+fn parse_state(input: &str) -> IResult<&str, (usize, State)> {
+    let (input, state) = preceded(
+        take_until("In state "),
+        preceded(tag("In state "), take(1usize)),
+    )(input)?;
+    let state = state_to_id(state);
+    let (input, zero_rule) = parse_rule(input)?;
+    let (input, one_rule) = parse_rule(input)?;
+
+    Ok((input, (state, (zero_rule, one_rule))))
+}
+
+fn parse_input(input: &str) -> IResult<&str, (usize, usize, Vec<State>)> {
+    let (input, begin) = preceded(tag("Begin in state "), take(1usize))(input)?;
+    let (input, steps) = preceded(
+        take_till(|c: char| c.is_numeric()),
+        take_till(|c: char| c == ' '),
+    )(input)?;
+    let begin = state_to_id(begin);
+    let steps = steps.parse().unwrap();
     dbg!(begin, steps);
-    for rows in input {
-        let mut rows = rows.lines();
-        if let Some(first) = rows.next() {
-            if let Some(first) = first.strip_prefix("Begin in state ") {
-                begin = (first.bytes().next().unwrap() - b'A') as usize;
-            }
-        }
-    }
-    todo!()
+
+    println!("{:?}", parse_state(input));
+    Ok(("", (begin, steps, vec![])))
 }
 
 fn part1() -> Result<()> {
@@ -48,7 +82,7 @@ fn part1() -> Result<()> {
 fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-    let (begin, steps, states) = parse_input(&input);
+    let (_, (begin, steps, states)) = parse_input(&input)?;
 
     // part1()?;
     // part2()?;
@@ -79,5 +113,5 @@ In state B:
     - Write the value 1.
     - Move one slot to the right.
     - Continue with state A.";
-    let (begin, steps, states) = parse_input(&input);
+    let (begin, steps, states) = parse_input(&input).unwrap();
 }
